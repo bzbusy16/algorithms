bb7c5a7924b43d61019276795537960da5fb45af
BZbusy <bzbusy16@gmail.com>
2025-01-02 20:08:33 +0800

    treeArray.cpp: Encapsulate the code into a class.

--- treeArray.cpp
+++ treeArray.cpp
@@ -11,45 +11,56 @@ using namespace std;
 可以看做线段树的简化版
 */
 
-int lowbit(int x){
-    // 实际上是在求x二进制最低位的1对应的数
-    // 例如 6 110 对应 2 10
-    return x&(-1*x);
-}
+class TreeArray{
+    vector<int> tree;   // 树，下标从1开始，维护数组的前缀和
+    vector<int> nums;   // 数组，下标从0开始
 
-void updateNode(int *tree, int pos, int len, int k){    // 更新节点值(+k), len是数组长度
-    for(int i=pos;i<=len;i+=lowbit(i)){
-        tree[i]+=k;
+public:
+    int lowbit(int x){
+        // 实际上是在求x二进制最低位的1对应的数
+        // 例如 6 110 对应 2 10
+        return x&(-1*x);
     }
-}
 
-int queryPresum(int *tree, int pos){    // 查询从pos开始的前缀和[..., pos]
-    int sum=0;
-    for(int i=pos;i>0;i-=lowbit(i)){    // 注意必须默认tree下标从1开始, 否则将死循环
-        sum+=tree[i];
+    // 向外提供的接口都是接收数组下标(从0开始)
+    void updateNode(int idx, int k){    // 更新节点值(+k)
+        for(int i=idx+1;i<=nums.size();i+=lowbit(i)){   // 更新前缀和
+            tree[i]+=k;
+        }
+        nums[idx]+=k;   // 更新数组元素
     }
-    return sum;
-}
 
-int queryInterval(int *tree, int left, int right){  // 查询区间和(left, right]
-    int pre_l=queryPresum(tree, left);
-    int pre_r=queryPresum(tree, right);
-    return pre_r-pre_l;
-}
+    int preSum(int idx){    // 获取前缀和 [0 ... idx]
+        int sum=0;
+        for(int i=idx+1;i>0;i-=lowbit(i)){    // 注意必须默认tree下标从1开始, 否则将死循环
+            sum+=tree[i];
+        }
+        return sum;
+    }
+
+    int getSum(int left, int right){  // 获取区间和 [left, right]
+        int lsum=preSum(left-1);
+        int rsum=preSum(right);
+        return rsum-lsum;
+    }
+
+    TreeArray(vector<int> &nums_): nums(nums_), tree(nums_.size()+1, 0){
+        for(int i=0;i<nums.size();++i) updateNode(i, nums[i]);
+    }
+};
 
 int main(){
-    vector<int> nums={3, 9, 0, 1, 2, -5, 4, 2, -6};
-    int *tree=new int[nums.size()+1]();
-    for(int i=0;i<nums.size();++i){
-        updateNode(tree, i+1, nums.size(), nums[i]);
-    }
+    vector<int> nums={7, 2, 7, 2, 0};
+    TreeArray tree_array(nums);
+    tree_array.updateNode(4, 6); // nums[4]+=6;
+    tree_array.updateNode(0, -5); // nums[0]+=-5
+    tree_array.updateNode(0, 7); // nums[0]+=7
+    cout << tree_array.getSum(4, 4) << endl;
+    tree_array.updateNode(3, 6); // nums[3]+=6
+    cout << tree_array.getSum(0, 4) << endl;
+    tree_array.updateNode(4, -5); // nums[4]+=-5
+    cout << tree_array.getSum(0, 3) << endl;
+    cout << tree_array.getSum(0, 4) << endl;
 
-    for(int i=1;i<=nums.size();++i){
-        cout << queryPresum(tree, i) << " ";
-    }
-    cout << endl;
-
-    cout << queryInterval(tree, 1, 5) << endl;
-    cout << queryInterval(tree, 6, 9) << endl;
     return 0;
 }
